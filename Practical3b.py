14de4c56da6ea0740addf181ca168ae80113ad55
not populated <dmckayk@cirrus-login1.ib0.icexa.epcc.ed.ac.uk>
2022-11-11 11:24:19 +0000

    Finished Practical 3 except for Learning Outcomes.

--- Practical3b.py
+++ Practical3b.py
@@ -9,10 +9,11 @@ import numpy as np
 
 # Import Dask dataframe
 
-
+import dask
 from dask import dataframe as dd
-
-
+from tornado.iostream import StreamClosedError
+from distributed.comm.core import CommClosedError
+dask.config.set({"distributed.comm.timeouts.tcp": "50s"})
 # Other imports
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
@@ -23,34 +24,20 @@ import sys
 
 from dask.distributed import Client, performance_report
 
-# def gen_plot(cols,dtypes,divisions):
-#     df = dd.read_csv('/work/m22oc/m22oc/shared/DAwHPC/practicals_data/P3/*2019.csv', usecols=cols, dtype=dtypes, parse_dates=['DATE_TIME']).replace(-9999,np.nan).set_index('DATE_TIME',divisions=divisions)
-#     print(df.head())
-#     df['SOIL_TEMP'] = df[['TDT1_TSOIL','TDT2_TSOIL']].mean(axis=1)
-#     df = df.drop(columns=['TDT1_TSOIL','TDT2_TSOIL']).compute().persist()
-
-#     df = df[:].resample('1d').mean().compute()
-
-#     ax = df.plot()
-#     return ax
+all_start = time.time()
 
 if __name__ == '__main__':
     start = time.time()
     client = Client(n_workers=36,threads_per_worker=2,memory_limit="{}GB".format(256//36))
     stop = time.time()
-    print("Dask setup time ",stop - start,"seconds")
+    print("Dask setup time: ",stop - start,"seconds")
     from dask.distributed import get_client
-
-    print(client.scheduler_info())
-    print(client)
 
     # a list of the columns we are interested in - we'll look at the air temperature and the soil temperature closest to the surface
     cols = ['DATE_TIME','SITE_ID','TA','TDT1_TSOIL','TDT2_TSOIL'] # list of columns we're interested in
     dtypes = {'DATE_TIME':object,'SITE_ID':str,'TA':np.float64,'TDT1_TSOIL':np.float64,'TDT2_TSOIL':np.float64} # datatypes dictionary
-    print(cols,dtypes)
-    # divisions = tuple(pd.date_range(start='2015', end='2020', freq='1d'))
-    # print(divisions)
-    with performance_report(filename='36workers_2threadsper_36workers.html'):
+    # print(cols,dtypes)
+    with performance_report(filename='Practical3b_performance.html'):
         start = time.time()
 
         ##
@@ -63,11 +50,16 @@ if __name__ == '__main__':
 
         df = df[:].resample('1d').mean()
         stop = time.time()
-        print('read csv',stop-start)
+        print('Data cleaning time: ',stop-start,' seconds.')
         start = time.time()
         ax = df.plot()
-        #futures = client.map(gen_plot,cols,dtypes,divisions)
-        #ax = client.gather(futures)
         stop = time.time()
-        print("Plot generation took ",stop - start," seconds.")
-    plt.savefig('test.png')
+        print("Plot generation time: ",stop - start," seconds.")
+    plt.savefig('Practical3b_plot.png')
+
+    all_stop = time.time()
+
+    print("Total run time: ",all_stop-all_start," seconds.")
+
+    # sys.exit() required inside __main__ to prevent Dask -> tornado communication attempts after the tornado client has closed (causing StreamClosedError)
+    sys.exit()
--- Practical3c.py
+++ Practical3c.py
@@ -2,16 +2,17 @@
 # coding: utf-8
 
 # Import Pandas and NumPy
-print('in')
+
 import pandas as pd
 import numpy as np
 
 
 # Dask imports
+import dask
 from dask import dataframe as dd
 from dask_jobqueue import SLURMCluster
 from dask.distributed import Client, performance_report
-
+dask.config.set({"distributed.comm.timeouts.tcp": "50s"})
 # Other imports
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
@@ -20,57 +21,64 @@ warnings.filterwarnings("ignore")
 import time
 import sys
 
-# if __name__ == '__main__':
-start = time.time()
-cluster = SLURMCluster(cores=12,
-                       processes=12,
-                       job_cpu=36,
-                       name='plotgen',
-                       memory='256GB',
-                       queue='standard',
-                       header_skip=['--mem'],
-                       job_extra=['--qos="standard"','--exclusive'],
-                       python='time srun python',
-                       account='m22oc-staff',
-                       walltime="00:10:00",
-                       shebang="#!/bin/bash --login",
-                       local_directory='$PWD',
-                       interface='ib0',
-                       log_directory='sched_logs',
-                       env_extra=['source /mnt/lustre/indy2lfs/work/m22oc/m22oc/dmckayk/.bashrc_node','conda activate'])
-cluster.scale(jobs=2)
-print('Cluster initiated')
-client = Client(cluster)
-print('Client initiated')
-stop = time.time()
-print("Dask setup time ",stop - start,"seconds")
-from dask.distributed import get_client
+all_start = time.time()
 
-print(client.scheduler_info())
-print(client)
-# a list of the columns we are interested in - we'll look at the air temperature and the soil temperature closest to the surface
-cols = ['DATE_TIME','SITE_ID','TA','TDT1_TSOIL','TDT2_TSOIL'] # list of columns we're interested in
-dtypes = {'DATE_TIME':object,'SITE_ID':str,'TA':np.float64,'TDT1_TSOIL':np.float64,'TDT2_TSOIL':np.float64} # datatypes dictionary
-print(cols,dtypes)
-with performance_report(filename='cluster.html',start=0):
+number_of_nodes = 1
+
+if __name__ == '__main__':
     start = time.time()
-    ##
-    df = dd.read_csv('/work/m22oc/m22oc/shared/DAwHPC/practicals_data/P3/*2019.csv', usecols=cols, dtype=dtypes, parse_dates=['DATE_TIME'])
-    start = time.time()
-    df = df.replace(-9999,np.nan).set_index('DATE_TIME',npartitions=72)
-
-
-    df['SOIL_TEMP'] = df[['TDT1_TSOIL','TDT2_TSOIL']].mean(axis=1)
-    df = df.drop(columns=['TDT1_TSOIL','TDT2_TSOIL']).compute()
-
-    df = df[:].resample('1d').mean()
+    cluster = SLURMCluster(cores=2,
+                           processes=36,
+                           job_cpu=36,
+                           name='plotgen',
+                           memory='256GB',
+                           queue='standard',
+                           header_skip=['--mem'],
+                           job_extra=['--qos="standard"','--exclusive'],
+                           python='time srun python',
+                           account='m22oc-staff',
+                           walltime="00:10:00",
+                           shebang="#!/bin/bash --login",
+                           local_directory='$PWD',
+                           interface='ib0',
+                           log_directory='sched_logs',
+                           env_extra=['source /mnt/lustre/indy2lfs/work/m22oc/m22oc/dmckayk/.bashrc_node','conda activate'])
+    cluster.scale(jobs=number_of_nodes)
+    print('Cluster initiated')
+    client = Client(cluster)
+    print('Client initiated')
     stop = time.time()
-    print('read csv',stop-start)
-    start = time.time()
-    #print(df.head())
-    ax = df.plot()
-    #futures = client.map(gen_plot,cols,dtypes,divisions)
-    #ax = client.gather(futures)
-    stop = time.time()
-    print("Plot generation took ",stop - start," seconds.")
-plt.savefig('test.png')
+    print("Dask setup time: ",stop - start,"seconds")
+    from dask.distributed import get_client
+
+    # print(client.scheduler_info())
+    # print(client)
+    # a list of the columns we are interested in - we'll look at the air temperature and the soil temperature closest to the surface
+    cols = ['DATE_TIME','SITE_ID','TA','TDT1_TSOIL','TDT2_TSOIL'] # list of columns we're interested in
+    dtypes = {'DATE_TIME':object,'SITE_ID':str,'TA':np.float64,'TDT1_TSOIL':np.float64,'TDT2_TSOIL':np.float64} # datatypes dictionary
+    # print(cols,dtypes)
+    with performance_report(filename='Practical3c_performance.html'):
+        start = time.time()
+        ##
+        df = dd.read_csv('/work/m22oc/m22oc/shared/DAwHPC/practicals_data/P3/*2019.csv', usecols=cols, dtype=dtypes, parse_dates=['DATE_TIME'])
+        start = time.time()
+        df = df.replace(-9999,np.nan).set_index('DATE_TIME',npartitions=72)
+
+
+        df['SOIL_TEMP'] = df[['TDT1_TSOIL','TDT2_TSOIL']].mean(axis=1)
+        df = df.drop(columns=['TDT1_TSOIL','TDT2_TSOIL']).compute()
+
+        df = df[:].resample('1d').mean()
+        stop = time.time()
+        print('Data cleaning time: ',stop-start)
+        start = time.time()
+        ax = df.plot()
+        stop = time.time()
+        print("Plot generation time: ",stop - start," seconds.")
+    plt.savefig('Practical3c_plot.png')
+    
+    all_stop = time.time()
+    
+    print("Total time: ",all_stop-all_start," seconds.")
+    
+    sys.exit()
